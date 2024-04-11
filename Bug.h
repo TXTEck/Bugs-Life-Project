26b0e638d812abac0b9e6b68fca6791d5c46f69f
TXTEck <91459117+TXTEck@users.noreply.github.com>
2024-04-11 18:41:44 +0800

    Bug Class

--- Bug.h
+++ Bug.h
@@ -8,16 +8,33 @@ class Bug {
 protected:
     int id;
     pair<int, int> position;
-    int direction;
+    int direction; // 1 = North, 2 = East, 3 = South, 4 = West
     int size;
     bool alive;
     list<pair<int,int>> path;
 
 public:
-    Bug(int id, pair<int, int> position, int direction, int size, bool alive);
+    Bug(int id, pair<int, int> position, int direction, int size, bool alive)
+        : id(id), position(std::move(position)), direction(direction), size(size), alive(alive) {}
 
         virtual void move() = 0;
+        bool isWayBlocked();{
+            int x = position.first;
+            int y = position.second;
 
+            switch(direction){
+                case 1:
+                    return y == 0;
+                case 2:
+                    return x ==9;
+                case 3:
+                    return y == 9;
+                case 4:
+                    return x == 0;
+                default
+                    return false;
+            }
+        };
 };
 
 
